3af6c39c899cad6cb8fd7263bf38ea2634148cc7
Ítalo Rodrigo <italo.computation@gmail.com>
2024-07-22 16:44:26 -0400

    Updates .docker
    - dir .docker install

--- api-sys_produtos/routes/web.php
+++ api-sys_produtos/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Olá bem-vindo!";
+});
+
+Route::get('/teste', function () {
+    return "Olá Teste!";
 });
